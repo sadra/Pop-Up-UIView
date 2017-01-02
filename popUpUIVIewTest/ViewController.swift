c1eb219affd5c08b7659e6f9c5e16b139bc311f6
sadra amlashi <amlashi.sadra@gmail.com>
2017-01-02 13:11:05 +0330

    Initial Commit

---
+++ popUpUIVIewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  popUpUIVIewTest
+//
+//  Created by Sadra on 1/2/17.
+//  Copyright © 2017 isapanah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
