a507dfe69f3b64053f3959553ceb23f7f6c92b44
sadra amlashi <amlashi.sadra@gmail.com>
2017-01-02 13:35:42 +0330

    Add popUpViewController Class and Xib

---
+++ popUpUIVIewTest/popUpViewController.swift
@@ -0,0 +1,26 @@
+//
+//  popUpViewController.swift
+//  popUpUIVIewTest
+//
+//  Created by Sadra on 1/2/17.
+//  Copyright © 2017 isapanah. All rights reserved.
+//
+
+import UIKit
+
+class popUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
